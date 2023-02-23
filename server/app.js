2c603a46e0ee00b1a8b03d5a79ec2d1faa28d0ca
ysawyers <yondenj.sawyers09@gmail.com>
2023-02-22 20:17:26 -0500

    updated libs

--- server/app.js
+++ server/app.js
@@ -1,13 +1,10 @@
 import { Router } from "@ysawyers/insourcing-proxy";
-import axios from "axios";
+// import axios from "axios";
 
 const app = new Router(5000, "127.0.0.1");
 
-const demoRequestHandler = async (req, res) => {
-  for (let i = 0; i < 1000; i++) {
-    let x = i * 5;
-  }
-  res.end("hey there!");
+const demoRequestHandler = (req, res) => {
+  res.end("I am here!");
 };
 
 app.get("/data/add", demoRequestHandler);
